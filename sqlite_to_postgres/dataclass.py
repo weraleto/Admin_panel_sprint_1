406945bdffe9b09bf716bca29d0ed7e2b42ef29d
weraleto <52561921+weraleto@users.noreply.github.com>
2021-09-04 14:53:08 +0300

    block 2

---
+++ sqlite_to_postgres/dataclass.py
@@ -0,0 +1,64 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+import uuid
+
+
+@dataclass(frozen=True)
+class Genre:
+    name: str
+    description: str = field(default='')
+    created_at: datetime = field(default_factory=datetime.now)
+    updated_at: datetime = field(default_factory=datetime.now)
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+
+
+@dataclass(frozen=True)
+class Person:
+    full_name: str
+    birth_date: datetime = None
+    created_at: datetime = field(default_factory=datetime.now)
+    updated_at: datetime = field(default_factory=datetime.now)
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+
+
+@dataclass(frozen=True)
+class Film:
+    title: str
+    description: str = field(default='')
+    creation_date: datetime = None
+    certificate: str = field(default='')
+    file_path: str = field(default='')
+    rating: float = field(default=0.0)
+    type: str = field(default='')
+    created_at: datetime = field(default_factory=datetime.now)
+    updated_at: datetime = field(default_factory=datetime.now)
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+
+
+@dataclass(frozen=True)
+class FilmGenre:
+    film_work_id: uuid.UUID
+    genre_id: uuid.UUID
+    created_at: datetime = field(default_factory=datetime.now)
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+
+
+@dataclass(frozen=True)
+class PersonFilmWork:
+    film_work_id: uuid.UUID
+    person_id: uuid.UUID
+    role: str
+    created_at: datetime = field(default_factory=datetime.now)
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+
+
+tables_dict = {
+    'genre': Genre,
+    'person': Person,
+    'film_work': Film,
+    'genre_film_work': FilmGenre,
+    'person_film_work': PersonFilmWork
+}
+
+if __name__ == '__main__':
+    pass
--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -1,20 +1,77 @@
 import sqlite3
+from dataclasses import asdict, fields
 
 import psycopg2
 from psycopg2.extensions import connection as _connection
-from psycopg2.extras import DictCursor
+from psycopg2.extras import DictCursor, execute_batch
+from dataclass import *
+
+
+class PostgresSaver:
+    def __init__(self, connection, schema):
+        self.cur = connection.cursor()
+        self._conn = connection
+        self._schema = schema
+
+    def truncate_data(self, table_name):
+        self.cur.execute(f'TRUNCATE content.{table_name}')
+
+    def save_data(self, table_name, field_list, data_obj):
+        field_list_query = ', '.join(field_list)
+        values_args_count = ', '.join(['%s'] * len(field_list))
+        values_list_query = list(data_obj.values())
+
+        execute_batch(
+            self.cur,
+            f'INSERT INTO {self._schema}.{table_name} ({field_list_query}) VALUES ({values_args_count}) '
+            f'ON CONFLICT (id) DO NOTHING',
+            [
+                values_list_query
+            ],
+        )
+
+
+class SQLiteLoader:
+    def __init__(self, connection):
+        self.cur = connection.cursor()
+
+    def load_data(self, table_name, field_list):
+        field_list = ', '.join(field_list)
+        self.cur.execute(f'SELECT {field_list} FROM {table_name}')
+        for row in self.cur:
+            yield row
 
 
 def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
     """Основной метод загрузки данных из SQLite в Postgres"""
-    # postgres_saver = PostgresSaver(pg_conn)
-    # sqlite_loader = SQLiteLoader(connection)
+    DEFAULT_SCHEMA = 'content'
+    postgres_saver = PostgresSaver(pg_conn, DEFAULT_SCHEMA)
+    sqlite_loader = SQLiteLoader(connection)
 
-    # data = sqlite_loader.load_movies()
-    # postgres_saver.save_all_data(data)
+    num = 1
+    for table_name, class_obj in tables_dict.items():
+        postgres_saver.truncate_data(table_name)
+        field_list = [i.name for i in fields(class_obj)]
+        query = sqlite_loader.load_data(table_name, field_list)
+        i = 0
+        for i, data in enumerate(query, 1):
+            data_obj = {key: value for key, value in zip(field_list, data) if value}
+            data_dcobj = asdict(class_obj(**data_obj))
+            postgres_saver.save_data(table_name, field_list, data_dcobj)
+            num += 1
 
 
 if __name__ == '__main__':
-    dsl = {'dbname': 'movies_database', 'user': 'postgres', 'password': 1234, 'host': '127.0.0.1', 'port': 5432}
+    dsl = {
+        'dbname': 'movies',
+        'user': 'postgres',
+        'host': 'localhost',
+        'password': 'postgres123',
+        'port': '5432',
+        'options': '-c search_path=content',
+    }
     with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
         load_from_sqlite(sqlite_conn, pg_conn)
+
+    sqlite_conn.close()
+    pg_conn.close()
