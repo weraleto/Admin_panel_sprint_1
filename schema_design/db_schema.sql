abd6894d32596f114f78419c814180477d126f8b
weraleto <52561921+weraleto@users.noreply.github.com>
2021-09-06 14:24:33 +0300

    fix idx names with _idx postfix

--- schema_design/db_schema.sql
+++ schema_design/db_schema.sql
@@ -44,6 +44,6 @@ CREATE TABLE IF NOT EXISTS content.person_film_work (
     created_at timestamp with time zone
 );
 
-CREATE UNIQUE INDEX film_work_genre ON content.genre_film_work (film_work_id, genre_id);
+CREATE UNIQUE INDEX film_work_genre_idx ON content.genre_film_work (film_work_id, genre_id);
 
-CREATE UNIQUE INDEX film_work_person ON content.person_film_work (film_work_id, person_id, role);
+CREATE UNIQUE INDEX film_work_person_idx ON content.person_film_work (film_work_id, person_id, role);
