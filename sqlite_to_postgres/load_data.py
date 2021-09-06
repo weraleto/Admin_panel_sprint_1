dc7a3ce453dfd63d3785780cce3c3faccd0edb4d
weraleto <52561921+weraleto@users.noreply.github.com>
2021-09-06 20:22:55 +0300

    optimize insert, add batch loading

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -1,10 +1,10 @@
 import sqlite3
-from dataclasses import asdict, fields
+from dataclasses import fields, astuple
 import os
 
 import psycopg2
 from psycopg2.extensions import connection as _connection
-from psycopg2.extras import DictCursor, execute_batch
+from psycopg2.extras import DictCursor, execute_values
 from dataclass import *
 
 
@@ -17,30 +17,24 @@ class PostgresSaver:
     def truncate_data(self, table_name):
         self.cur.execute(f'TRUNCATE {self._schema}.{table_name}')
 
-    def save_data(self, table_name, field_list, data_obj):
-        field_list_query = ', '.join(field_list)
-        values_args_count = ', '.join(['%s'] * len(field_list))
-        values_list_query = list(data_obj.values())
-
-        execute_batch(
+    def save_data(self, table_name, field_list, data):
+        execute_values(
             self.cur,
-            f'INSERT INTO {self._schema}.{table_name} ({field_list_query}) VALUES ({values_args_count}) '
+            f'INSERT INTO {self._schema}.{table_name} ({field_list}) VALUES %s '
             f'ON CONFLICT (id) DO NOTHING',
-            [
-                values_list_query
-            ],
+            data,
         )
 
 
 class SQLiteLoader:
     def __init__(self, connection):
-        self.cur = connection.cursor()
+        self._conn = connection
+        self._conn.row_factory = sqlite3.Row
+        self.cur = self._conn.cursor()
 
     def load_data(self, table_name, field_list):
-        field_list = ', '.join(field_list)
         self.cur.execute(f'SELECT {field_list} FROM {table_name}')
-        for row in self.cur:
-            yield row
+        return self.cur
 
 
 def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection, default_schema):
@@ -48,16 +42,17 @@ def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection, defau
     postgres_saver = PostgresSaver(pg_conn, default_schema)
     sqlite_loader = SQLiteLoader(connection)
 
-    num = 1
     for table_name, class_obj in tables_dict.items():
-        field_list = [i.name for i in fields(class_obj)]
+        field_list = ', '.join([i.name for i in fields(class_obj)])
         query = sqlite_loader.load_data(table_name, field_list)
-        i = 0
-        for i, data in enumerate(query, 1):
-            data_obj = {key: value for key, value in zip(field_list, data) if value}
-            data_dcobj = asdict(class_obj(**data_obj))
-            postgres_saver.save_data(table_name, field_list, data_dcobj)
-            num += 1
+
+        while True:
+            data_values = [
+                astuple(class_obj(**i)) for i in query.fetchmany(800)
+            ]
+            if not data_values:
+                break
+            postgres_saver.save_data(table_name, field_list, data_values)
 
 
 if __name__ == '__main__':
