73a1cfb628345020a32facf0e8a12829f8fa370a
weraleto <52561921+weraleto@users.noreply.github.com>
2021-09-06 22:38:40 +0300

    add search_fields

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -13,20 +13,24 @@ class PersonFilmworkAdmin(admin.TabularInline):
 @admin.register(Filmwork)
 class FilmworkAdmin(admin.ModelAdmin):
     list_display = ('title', 'type', 'creation_date', 'rating')
+    search_fields = ('title',)
+    list_filter = ('type',)
     inlines = [
         FilmworkGenreAdmin, PersonFilmworkAdmin
     ]
+    readonly_fields = ('created_at', 'updated_at')
     fields = (
         'title', 'type', 'description', 'creation_date', 'certificate',
-        'file_path', 'rating'
+        'file_path', 'rating', 'created_at', 'updated_at'
     )
 
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'birth_date')
+    search_fields = ('full_name',)
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ('name',)
